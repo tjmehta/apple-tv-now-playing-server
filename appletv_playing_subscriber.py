1538ee3520899f06bac997d51540b61595f10c06
Tejesh Mehta <tejesh.mehta@gmail.com>
2023-08-29 23:21:27 -0700

    added ping

--- appletv_playing_subscriber.py
+++ appletv_playing_subscriber.py
@@ -13,9 +13,28 @@ class AppletvPlayingSubscriber:
         self.atv = atv
         self.atv.push_updater.listener = push_listener
         self.atv.device_info.listener = listener
+        self.ping_interval = 60 * 30  # 30 min in seconds
+        self.ping_task = None
+
+    async def start_ping(self):
+        while True:
+            await self.ping_atv()
+            await asyncio.sleep(self.ping_interval)
+
+    async def ping_atv(self):
+        await self.atv.metadata.playing()
+
+    def handle_ping_task_exception(self, task):
+        if task.cancelled():
+            print("Ping task was cancelled")
+        elif task.exception() is not None:
+            raise Exception("Ping task raised an exception:", task.exception())
 
     def start(self):
         self.atv.push_updater.start()
+        self.ping_task = asyncio.ensure_future(self.start_ping())
+        self.ping_task.add_done_callback(self.handle_ping_task_exception)
 
     def stop(self):
         self.atv.push_updater.stop()
+        self.ping_task.cancel()
